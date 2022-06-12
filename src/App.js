49281037eb70c879eb04ec54a93c6b8a50b536e7
Dave <daveslaw@gmail.com>
2022-06-12 20:18:52 +0300

    setting up clothing depts / mapping

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-     Hello world
-    </div>
-  );
-}
+const App = () => {
+	const deptArray = [
+		{
+			id: "1",
+			name: "Hats",
+		},
+		{
+			id: "2",
+			name: "Jackets",
+		},
+		{
+			id: "3",
+			name: "Women",
+		},
+		{
+			id: "4",
+			name: "Men",
+		},
+		{
+			id: "5",
+			name: "Sneakers",
+		},
+	];
+
+	return (
+		<div className="departments-container">
+      {deptArray.map((department => (
+        <div className="department-container" key={department.id}>
+				{/* <img /> */}
+				<div className="department-body-container">
+					<h2>{department.name}</h2>
+					<p>Shop now</p>
+				</div>
+			</div>
+      )))}
+			
+		</div>
+	);
+};
 
 export default App;
