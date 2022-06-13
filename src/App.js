e2be05597df08acb13c5fe789e894405c73e9734
Dave <daveslaw@gmail.com>
2022-06-13 19:44:35 +0300

    navbar with routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import "./App.css";
 import Home from "./routes/Home/Home";
-import { Route, Routes, Outlet } from "react-router-dom";
+import NavBar from './routes/NavBar/NavBar'
+import { Route, Routes } from "react-router-dom";
 
 
-
-const NavBar = () => {
+const Shop = () => {
 	return (
-		<div>
-			<div><h2>NavBar</h2></div>
-			<Outlet/>
-		</div>
+		<div>I am the shop</div>
 	)
 }
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<NavBar />}>
-				<Route index element={<Home />}/>
-
-				
-
-
+			<Route path="/" element={<NavBar />}>
+				<Route index element={<Home />} />
+				<Route path='shop' element={<Shop/>}/>
 			</Route>
-
-		
 		</Routes>
 	);
 };
---
+++ src/routes/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import { React, Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+import './navbar.styles.scss';
+
+const NavBar = () => {
+	return (
+		<>
+			<div className="navbar">
+				<Link to='/' className='logo-container'><CrownLogo className='logo'/></Link>
+                <div className="nav-links-container">
+                    <Link className='nav-link' to='/shop'>SHOP</Link>
+                </div>
+			</div>
+			<Outlet />
+		</>
+	);
+};
+
+export default NavBar;
