ad3d83d1c03e04d535a363f1f85475729464d1d0
Dave <daveslaw@gmail.com>
2022-06-21 23:15:17 +0300

    different routing pattern

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import "./App.css";
 import Home from "./routes/Home/Home";
-import NavBar from './routes/NavBar/NavBar';
-import Shop from '../src/routes/Shop/Shop'
-import Authentication from './routes/Authentication/Authentication';
+import NavBar from "./routes/NavBar/NavBar";
+import Shop from "../src/routes/Shop/Shop";
+import Authentication from "./routes/Authentication/Authentication";
 import { Route, Routes } from "react-router-dom";
-import Checkout from './routes/Checkout/Checkout'
-
-
-
+import Checkout from "./routes/Checkout/Checkout";
 
 const App = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<NavBar />}>
+		<>
+			<NavBar />
+			<Routes>
 				<Route index element={<Home />} />
-				<Route path='shop/*' element={<Shop/>}/>
-				<Route path='auth' element={<Authentication />}/>
-				<Route path='checkout' element={<Checkout/>}/>
-
-
-			</Route>
-		</Routes>
+				<Route path="shop/*" element={<Shop />} />
+				<Route path="auth" element={<Authentication />} />
+				<Route path="checkout" element={<Checkout />} />
+			</Routes>
+		</>
 	);
 };
 
