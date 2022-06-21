63686c719f87b2f363038b67774cb2573e972640
Dave <daveslaw@gmail.com>
2022-06-21 11:40:41 +0300

    some styling touch ups - removed red border in authorisation page

--- src/routes/Authentication/authentication.route.js
+++ src/routes/Authentication/authentication.route.js
@@ -3,6 +3,5 @@ import styled from "styled-components";
 export const AuthenticationContainer = styled.div`
 	display: flex;
 	flex-flow: row;
-	border: 1px solid red;
 	justify-content: space-evenly;
 `;
